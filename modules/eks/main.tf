cbfb73ac826b410833ae8b8747936a70f23a617a
Bilal Khan <bilalstanz@gmail.com>
2026-02-15 19:10:07 +0530

    added module logic

---
+++ modules/eks/main.tf
@@ -0,0 +1,13 @@
+resource "aws_eks_cluster" "this" {
+
+  name     = var.cluster_name
+  role_arn = var.cluster_role_arn
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
+
+  version = var.cluster_version
+
+  tags = var.tags
+}
